bb0d73ce94d68ba4163c0fbcd34f126399815592
ullahkun <ullahkun@gmail.com>
2021-06-19 19:13:06 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors:{
+      'pink': '#EA4C89',
+      'darkblue': '#49516F',
+      'gray':'#696871',
+      'white': '#fff',
+      'orange':'#FF7143',
+      'darkorange':'#EB5A2B',
+      'black':'#19191B',
+      'lightgray' :'#EDF2F7',
+      'brown':'#FEF2EF',
+      'dark':'#121927',
+      'lightbrown' : '#FFF9F4',
+      'gold' : '#D8AD84',
+      'lightgray' : '#54595F'
+
+      
+
+    },
+    fontFamily: {
+      'sans': ['DM Sans'],
+      'cour': ['Courgette'],
+      'play': ['Playfair Display'],
+      'nama': ['Tangerine'],
+     },
+
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
